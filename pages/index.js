ee3f91f5ec5b3ee3f634182c156c9cae7f61048a
Thomas Gak Deluen <tgdn45@gmail.com>
2020-03-31 10:00:36 +0100

    Initial commmit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Input = (props) => (
+  <input {...props} />
+);
+
+export default () => {
+  const router = useRouter();
+  const { q = '' } = router.query;
+  console.log(q)
+
+  const onChange = (e) => {
+    const url = String(`/?q=${e.target.value}`);
+    router.push(url, null, { shallow: true });
+  }
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Input defaultValue={q} onChange={onChange} />
+    </>
+  )
+}
